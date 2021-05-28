b5117d696d6dfa5cf1191b1cd59a11b37c866aea
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-28 17:33:22 -0600

    Update SpriteSheet.hooks.php

--- SpriteSheet.hooks.php
+++ SpriteSheet.hooks.php
@@ -509,7 +509,7 @@ class SpriteSheetHooks {
 		$canEdit = true;
 		if (
 			!$services->getPermissionManager()->userHasRight( $wgUser, 'edit_sprites' ) ||
-			!$services->getPermissionManager()->userCan( 'edit', $wgUser, $imagePage )
+			!$services->getPermissionManager()->userCan( 'edit', $wgUser, $imagePage->getTitle() )
 		) {
 			$canEdit = false;
 		}
